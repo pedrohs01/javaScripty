707a661c284e63b072eac37990e4866265d4a74e
PEDRO HENRIQUE DOS SANTOS <pedrosantosjovem2004@gmail.com>
2024-05-07 17:27:34 -0300

    novas js

--- exercicios/media/calc.js
+++ exercicios/media/calc.js
@@ -2,25 +2,35 @@
 @author PEDRO
 */
 
-let n1, n2, n3, n4 
-function calcular() {
-    n1 = Number(frmflex.txtn1.value)   
-    n2 = Number(frmflex.txtn2.value) 
-    n3 = Number(frmflex.txtn3.value)
-    n4 = Number(frmflex.txtn4.value)
+let nota1, nota2, nota3, nota4
 
-    //validação de campos obrigatorio
-    if (frmflex.txtn1.value === "") {
-        alert("prencha o valor da n1")
-        frmflex.txtn1.value.focus()
-    } else if (frmflex.txtn2.value === "") {
-        alert("prencha o valor da n2")
-        frmflex.txtn2.value.focus()
-    } else if (frmflex.txtn3.value === "") {
-        alert("prencha o valor da n3")
-        frmflex.txtn3.value.focus()
-    } else if (frmflex.txtn4.value === "") {
-        alert("prencha o valor da n4")
-        frmflex.txtn4.value.focus() }
-    
+
+function calcular() {
+    nota1 = Number(frmMedia.txtn1.value.replace(",", "."))
+    nota2 = Number(frmMedia.txtn2.value.replace(",", "."))
+    nota3 = Number(frmMedia.txtn3.value.replace(",", "."))
+    nota4 = Number(frmMedia.txtn4.value.replace(",", "."))
+
+    const resultado = (nota1 + nota2 + nota3 + nota4) / 4
+
+    if (frmMedia.txtn1.value === "") {
+        alert("Preencha o valor da nota")
+    } else if (frmMedia.txtn2.value === "") {
+        alert("Preencha o valor da nota")
+    } else if (frmMedia.txtn3.value === "") {
+        alert("Preencha o valor da nota")
+    } else if (frmMedia.txtn4.value === "") {
+        alert("Preencha o valor da nota")
+    }else {
+        if(resultado < 4 ) {
+            document.getElementById('status').src = "./img/reprovado.jpg"
+            alert(`A média do aluno é ${resultado}, ele está reprovado!`)
+        }else if (resultado >= 7){
+            document.getElementById('status').src = "./img/aprovado.jpg"
+            alert(`A média do aluno é ${resultado}, ele está aprovado!`)
+        }else {
+            document.getElementById('status').src = "./img/recuperacao.jpg"
+            alert(`A média do aluno é ${resultado}, ele está de recuperação!`)
+        }
     }
+}
